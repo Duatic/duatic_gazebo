572513bd5fa100d0752a0839c25e5f36be5fb131
Marc Blöchlinger <91188043+mbloechli@users.noreply.github.com>
2025-11-24 15:35:53 +0100

    refactor(launch): accomodate forwarding of namespace (#6)

--- launch/spawn.launch.py
+++ launch/spawn.launch.py
@@ -7,31 +7,29 @@ ARGUMENTS = [
     DeclareLaunchArgument(
         "namespace", default_value="empty_namespace", description="entity namespace"
     ),
-    DeclareLaunchArgument("x", default_value="0.0", description="x position"),
-    DeclareLaunchArgument("y", default_value="0.0", description="y position"),
-    DeclareLaunchArgument("z", default_value="0.0", description="z position"),
-    DeclareLaunchArgument("yaw", default_value="0.0", description="yaw rotation"),
+    DeclareLaunchArgument("initial_pose_x", default_value="0.0", description="x position"),
+    DeclareLaunchArgument("initial_pose_y", default_value="0.0", description="y position"),
+    DeclareLaunchArgument("initial_pose_z", default_value="0.0", description="z position"),
+    DeclareLaunchArgument("initial_pose_yaw", default_value="0.0", description="yaw rotation"),
 ]
 
 
 def generate_launch_description():
-
     spawn_entity = Node(
         package="ros_gz_sim",
-        namespace=LaunchConfiguration("namespace"),
         name="create",
         executable="create",
         arguments=[
             "-name",
             LaunchConfiguration("namespace"),
             "-x",
-            LaunchConfiguration("x"),
+            LaunchConfiguration("initial_pose_x"),
             "-y",
-            LaunchConfiguration("y"),
+            LaunchConfiguration("initial_pose_y"),
             "-z",
-            LaunchConfiguration("z"),
+            LaunchConfiguration("initial_pose_z"),
             "-Y",
-            LaunchConfiguration("yaw"),
+            LaunchConfiguration("initial_pose_yaw"),
             "-topic",
             "robot_description",
         ],
