c26fb8487fedd265ecf8b049f26b2d65f3cae31d
mbloechli <mbloechlinger@duatic.com>
2026-02-26 18:38:41 +0100

    feat(launch): added parameters for changing gazebo resource paths

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -5,9 +5,10 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
-    IncludeLaunchDescription,
-    SetEnvironmentVariable,
     GroupAction,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+    SetEnvironmentVariable,
 )
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -16,20 +17,8 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 
 
-ARGUMENTS = [
-    DeclareLaunchArgument("world", default_value="empty", description="Simulation World"),
-    DeclareLaunchArgument(
-        "headless",
-        default_value="false",
-        choices=["false", "true"],
-        description="Run the simulation headless",
-    ),
-]
-
-
-def generate_launch_description():
-    # Paths
-    pkg_duatic_gazebo = get_package_share_directory("duatic_gazebo")
+def launch_setup(context, *args, **kwargs):
+    # Packages Directories
     pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
     gz_sim_launch = PathJoinSubstitution([pkg_ros_gz_sim, "launch", "gz_sim.launch.py"])
@@ -37,16 +26,15 @@ def generate_launch_description():
     # Set Gazebo resource path
     gz_resource_path = SetEnvironmentVariable(
         name="GZ_SIM_RESOURCE_PATH",
-        value=":".join(
-            [
-                os.path.join(pkg_duatic_gazebo, "worlds"),
-                os.path.join(pkg_duatic_gazebo, "models"),
-            ]
-        ),
+        value=[
+            LaunchConfiguration("gz_worlds_path"),
+            ":",
+            LaunchConfiguration("gz_models_path"),
+        ],
     )
 
     # Launch Gazebo headless or with GUI
-    gz_args = [LaunchConfiguration("world"), ".sdf", " -r", " -v 2"]
+    gz_args = [LaunchConfiguration("world"), ".sdf", " -r", " -v", LaunchConfiguration("log_level")]
 
     gazebo = GroupAction(
         [
@@ -72,9 +60,35 @@ def generate_launch_description():
         arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"],
     )
 
-    # Compose launch description
-    ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(gz_resource_path)
-    ld.add_action(gazebo)
-    ld.add_action(clock_bridge)
-    return ld
+    return [gz_resource_path, gazebo, clock_bridge]
+
+
+def generate_launch_description():
+    # Declare Launch Arguments
+    declared_arguments = [
+        DeclareLaunchArgument("world", default_value="empty", description="Simulation World"),
+        DeclareLaunchArgument(
+            "headless",
+            default_value="false",
+            choices=["false", "true"],
+            description="Run the simulation headless",
+        ),
+        DeclareLaunchArgument(
+            "gz_worlds_path",
+            default_value=os.path.join(get_package_share_directory("duatic_gazebo"), "worlds"),
+            description="Path to the Gazebo worlds directory",
+        ),
+        DeclareLaunchArgument(
+            "gz_models_path",
+            default_value=os.path.join(get_package_share_directory("duatic_gazebo"), "models"),
+            description="Path to the Gazebo models directory",
+        ),
+        DeclareLaunchArgument(
+            "log_level",
+            default_value="1",
+            description="Gazebo log level(debug:4, info:3, warn:2, error:1, fatal:0)",
+        ),
+    ]
+
+    # Add nodes to LaunchDescription
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
